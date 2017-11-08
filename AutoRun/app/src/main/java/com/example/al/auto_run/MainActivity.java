1132646ae1cf29bd51f1c4f54883e4aab4e97595
Nilianfei <1074523347@qq.com>
2017-11-08 19:23:16 +0800

    空项目

---
+++ AutoRun/app/src/main/java/com/example/al/auto_run/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.al.auto_run;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
